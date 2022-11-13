54caf92c06a83133219fd8a7e0c2b1a7fc952bc5
Guillaume Ouellet <gulaou@gmail.com>
2022-11-13 14:52:37 +0800

    Minor UI updates
    - Cleanup script
    - New Scene for Blueprint inventory UI
    - New button to return to main screen

--- src/ui/gui.gd
+++ src/ui/gui.gd
@@ -1,2 +1,11 @@
 extends Control
 
+
+func _on_Button_Save_pressed() -> void:
+	SaveFile.save_game()
+
+
+func _on_Button_Exit_pressed() -> void:
+	var err = get_tree().change_scene("res://src/screens/main_menu.tscn")
+	if err != OK:
+		Log.log_error(err)
